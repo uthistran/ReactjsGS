798b6e632213b7202edce33afec4d182d74a9a55
Uthistran <uthistran@gmail.com>
2020-05-07 10:12:19 -0700

    Add files via upload

---
+++ precache-manifest.a24a0b29e091c4648eb35a8c3b194ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7567df1d182af44358472e90a33a7747",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf3e410bdf176b33c1f",
+    "url": "/static/css/main.3ed6fcf8.chunk.css"
+  },
+  {
+    "revision": "24b1f0377aca694dce96",
+    "url": "/static/js/2.7b11c0b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b11c0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf3e410bdf176b33c1f",
+    "url": "/static/js/main.9674f6d4.chunk.js"
+  },
+  {
+    "revision": "79e258edd92db2aa6145",
+    "url": "/static/js/runtime-main.aa44fd46.js"
+  }
+]);
